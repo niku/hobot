3bc7ca027f0daea051848117de15db90a5882b06
niku <niku@niku.name>
2016-07-08 12:57:34 +0900

    Publish to hex.pm and hexdocs.pm automatically if A CI has succeeded

--- mix.exs
+++ mix.exs
@@ -31,7 +31,7 @@ defmodule BeamHomunculus.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:ex_doc, ">= 0.0.0", only: :dev},
+    [{:ex_doc, ">= 0.0.0", only: [:dev, :test]},
      {:dialyxir, "~> 0.3.5", only: [:dev, :test]},
      {:credo, "~> 0.4", only: [:dev, :test]}]
   end
