5f8f584da3b15285e8a84323c1da9cb85eb45381
niku <niku@niku.name>
2017-06-04 19:42:24 +0900

    Add sample handler

---
+++ lib/hobot/handlers/echo.ex
@@ -0,0 +1,26 @@
+defmodule Hobot.Handlers.Echo do
+  @moduledoc """
+  Echoes input.
+  """
+
+  use GenServer
+
+  def start_link({topic, handler_options}, genserver_options \\ []) do
+    GenServer.start_link(__MODULE__, {topic, handler_options}, genserver_options)
+  end
+
+  def init({topic, _handler_options} = args) do
+    Hobot.subscribe(topic)
+    {:ok, args}
+  end
+
+  def handle_cast({:broadcast, from, ref, data}, state) do
+    GenServer.cast(from, {ref, data})
+    {:noreply, state}
+  end
+
+  def terminate(reason, {topic, _handler_options}) do
+    Hobot.unsubscribe(topic)
+    reason
+  end
+end
