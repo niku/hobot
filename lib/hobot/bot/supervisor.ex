af52e489532f114201f0b51f0566003a298196bd
niku <niku@niku.name>
2017-07-12 00:31:57 +0900

    Make easy to understand
    It inlines a function.

--- lib/hobot/bot/supervisor.ex
+++ lib/hobot/bot/supervisor.ex
@@ -11,8 +11,10 @@ defmodule Hobot.Bot.Supervisor do
       supervisor(Registry, [:duplicate, context.pub_sub]),
       # Right now, I don't implement to take a GenServer options because of I don't need it.
       # If you want give a GenServer options from outside, feel free to make a pull request.
-      worker(GenServer, [adapter.module, build_args(adapter, context), [name: context.adapter]]),
-    ] ++ build_handlers(handlers, context)
+      worker(GenServer, [adapter.module, build_args(adapter, context), [name: context.adapter]], [id: context.adapter]),
+    ] ++ for {handler, index} <- Enum.with_index(handlers) do
+      worker(GenServer, [handler.module, build_args(handler, context), []], [id: "Handler#{index}"])
+    end
 
     # NOTE: It's worth to add a process name. I couldn't it right now.
     opts = [strategy: :one_for_one]
@@ -23,12 +25,6 @@ defmodule Hobot.Bot.Supervisor do
     case Map.get(conf, :args, []) do
       [] -> context
       args -> {context, args}
-    end
-  end
-
-  def build_handlers(handlers, context) do
-    for {handler, index} <- Enum.with_index(handlers) do
-      worker(GenServer, [handler.module, build_args(handler, context)], [id: "Handler#{index}"])
     end
   end
 end
