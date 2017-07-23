827bfe2bdbf92a8fb4c7023d6a88ebb0a3f8e6c6
niku <niku@niku.name>
2017-07-24 08:47:55 +0900

    Remove a prediod before a index number
    Hobot.Bot.Foo.Handler.0 ->
    Hobot.Bot.Foo.Handler0

--- lib/hobot/handler.ex
+++ lib/hobot/handler.ex
@@ -2,7 +2,6 @@ defmodule Hobot.Handler do
   @name_suffix "Handler"
 
   def build_name(atom, index) do
-    Module.concat(atom, @name_suffix)
-    |> Module.concat(Integer.to_string(index))
+    Module.concat(atom, @name_suffix <> Integer.to_string(index))
   end
 end
