73b238ee1b4c6b416c79f98f78006f757fd67e49
niku <niku@niku.name>
2017-08-01 23:37:13 +0900

    Implement Hobot.create/4 to create a bot

--- lib/hobot.ex
+++ lib/hobot.ex
@@ -2,4 +2,18 @@ defmodule Hobot do
   @moduledoc """
   A bot framework for ErlangVM(beam)
   """
+
+  def create(name, adapter, handlers, options \\ []) do
+    name_registry = Keyword.get(options, :name_registry, Hobot.Application.name_registry())
+    pub_sub = Keyword.get(options, :pub_sub, Hobot.Application.pub_sub())
+    task_supervisor = Keyword.get(options, :task_supervisor, Hobot.Application.task_supervisor())
+
+    Supervisor.start_child(Hobot.Supervisor,
+      [%{name: name,
+         adapter: adapter,
+         handlers: handlers,
+         name_registry: name_registry,
+         pub_sub: pub_sub,
+         task_supervisor: task_supervisor}])
+  end
 end
